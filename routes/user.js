151800da7ecab81e814648cefb76e8cae29bbbb1
Mohamed Emara <medoemara104@gmail.com>
2023-10-31 23:10:44 +0200

    Add elements to view to display stories for each user

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,31 @@
 import express from "express";
 const router = express.Router();
 import { ensureAuth, ensureGuest } from "../middleware/auth.js";
+import { Story } from "../models/Story.js";
 
 // @desc    Login/Landing Page
 // @route   GET / 
-router.get('/dashboard', ensureAuth, (req, res) => {
+router.get('/dashboard', ensureAuth, async (req, res) => {
     console.log(req.user);
-    res.render('dashboard', {
-        // We can pass an object to be accessed in the renderred page.
-        name: req.user.firstName
-        // We simply can access it in double curly braces {{name}} in the view.
-         
-    })
+    try {
+        const stories = await Story.find({ auth: req.user._id }).lean();        // Get all stories which this user is thier Author.
+        
+        // After fetching the stories.
+        // We'll pass them to the template.
+        res.render('dashboard', {
+            // We can pass an object to be accessed in the renderred page.
+            name: req.user.firstName,
+            stories
+            // We simply can access it in double curly braces {{name}} in the view.
+    
+        })
+
+    } catch (err) {
+        console.log(err);
+        res.render('/errors/500');
+    }
+
+
 }); 
 
 
